c56d248c46b8dcdd9d2be6e871ab401bf1f30cb1
urmila1706 <raut.urmila88@gmail.com>
2023-04-01 23:40:44 +0530

    added title

--- app.js
+++
@@ -1,1 +0,0 @@
-console.log('I study in sharp')
---
+++ dom.js
@@ -0,0 +1,22 @@
+//console.dir(document);
+// console.log(document.domain);
+//console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.all);
+// console.log(document.all[10]);
+//console.log(document.getElementById('header-title'));
+//var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+//headerTitle.innerText = 'GoodBye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+//var header = document.getElementById('main-header');
+//header.style.borderBottom = 'solid 3px #000';
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+//titles[0].textContent = 'hello';
+titles[0].style.fontWeight = 'bold';
+titles[0].style.color = 'green';
+
+
