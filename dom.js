3aabb8642dac0ab5cfcbf57c0e2ec676c505f940
urmila1706 <raut.urmila88@gmail.com>
2023-04-03 14:38:24 +0530

    by queryselectorAll

--- dom.js
+++ dom.js
@@ -27,14 +27,17 @@
 //     items[i].style.fontWeight = 'bold';
 // }
 // var li = document.getElementsByTagName('li');
-//var item = document.querySelector('.list-group-item');
-//item.style.color = 'red';
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)')
-thirdItem.textContent = '';
-
-
+/// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)')
+// thirdItem.textContent = '';
+//var li= document.querySelectorAll('li');
+//li[1].style.backgroundColor = 'green';
+var odd= document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor = 'green';
+}
 // for(var i=0;i<li.length;i++)
 // {
 //     li[i].style.fontWeight = 'bold';
