7279a8ba7c58d62c9264c4123957998e08a5afa6
urmila1706 <raut.urmila88@gmail.com>
2023-04-07 23:02:34 +0530

    add delete and remove functionality

--- dom.js
+++ dom.js
@@ -1,51 +1,41 @@
-var itemList = document.querySelector('#items')
-console.log(itemList.parentNode);
-itemList.parentNode.style.backgroundColor = '#f4f4f4';
-console.log(itemList.parentNode.parentNode.parentNode);
-//#1.parent Element
-console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor = '#f4f4f4';
-console.log(itemList.parentElement.parentElement.parentElement);
-//#2.createChild
-console.log(itemList.childNodes);
-console.log(itemList.children[0]);
-itemList.children[2].style.backgroundColor = 'green';
-//#3.firstChild
-console.log(itemList.firstChild);
-//4.firstElementChild
-console.log(itemList.firstElementChild);
-itemList.firstElementChild.textContent = 'Hello';
-//#5.lastChild
-console.log(itemList.lastChild);
-//#6.lastElementChild
-console.log(itemList.lastElementChild);
-itemList.lastElementChild.textContent = 'Hello World';
-//#7.nextSibling
-console.log(itemList.nextSibling);
-//#8.nextElementSibling
-console.log(itemList.nextElementSibling);
-//#9.previousSibling
-console.log(itemList.previousSibling);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+form.addEventListener('submit' , addItem);
+itemList.addEventListener('click',removeItem);
+function addItem(e){
+    e.preventDefault();
+    
+    var newItem = document.getElementById('item');
+    var li = document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode('newItem'));
 
- itemList.previousElementSibling.style.color = 'red';
- itemList.previousElementSibling.textContent = 'HEllo word';
- //#10.previousElementSibling
-console.log(itemList.previousElementSibling);
-//#11.createElement
-var newDiv = document.createElement('div');
-console.log(newDiv);
-newDiv.className = 'hi';
-newDiv.id = 'hello2';
-//#12.setAttribute
-newDiv.setAttribute('title','Hello Div');
-//#13.createTextNode
-var newDivText= document.createTextNode('HEllo Word');
-//#14.appendChild
+let editButton = document.createElement("button");
+  //Adding The Classes to it
+  editButton.classList = "btn btn-danger btn-sm float-right";
+  //Adding the Text Content
+  editButton.appendChild(document.createTextNode("Edit"));
+  //Appending to List
+  li.appendChild(editButton);
 
-newDiv.appendChild(newDivText);
+  itemList.appendChild(li);
 
-var container =  document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+  var deleteBtn = document.createElement('button');
+deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+deleteBtn.appendChild(document.createTextNode('X'));
+li.appendChild(deleteBtn);
+itemList.appendChild(li);
 
-newDiv.style.fontSize = '30px';
-container.insertBefore(newDiv, h1);
+
+}
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+
+
